14446196396717317adde55b6ac2c3e25b65fe08
Jesus Alejandro Sanchez Davila <silvarion@gmail.com>
2021-07-12 17:33:44 +0200

    Pumped version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyoocass", 
-    version="0.0.1",
+    version="0.0.2",
     author="Jesus Alejandro Sanchez Davila",
     author_email="jsanchez.consultant@gmail.com",
     description="Python Object-Orinted Cassandra interface",
